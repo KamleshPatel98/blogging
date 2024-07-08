49de90202a6dbc56b903387856ab5de62ded09b5
KamleshPatel98 <kamleshp52170@gmail.com>
2024-07-08 23:52:45 +0530

    add website and admin panel with register and login page

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+    $conn=mysqli_connect('localhost','root','','blogging');
+    if(!$conn){
+        echo "Connection Failed!";
+    }
+?>
---
+++ header.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="megakit,business,company,agency,multipurpose,modern,bootstrap4">
+    
+    <meta name="author" content="themefisher.com">
+
+    <title>Blogging</title>
+
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="web/plugins/bootstrap/css/bootstrap.min.css">
+    <!-- Icon Font Css -->
+    <link rel="stylesheet" href="web/plugins/themify/css/themify-icons.css">
+    <link rel="stylesheet" href="web/plugins/fontawesome/css/all.css">
+    <link rel="stylesheet" href="web/plugins/magnific-popup/dist/magnific-popup.css">
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="web/plugins/slick-carousel/slick/slick.css">
+    <link rel="stylesheet" href="web/plugins/slick-carousel/slick/slick-theme.css">
+
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="web/css/style.css">
+</head>
+
+<body>
+
+
+<!-- Header Start --> 
+
+<header class="navigation">
+	<nav class="navbar navbar-expand-lg  py-4" id="navbar">
+		<div class="container">
+        <a class="navbar-brand" href="index.html">
+        Mega<span>kit.</span>
+        </a>
+
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="fa fa-bars"></span>
+        </button>
+    
+        <div class="collapse navbar-collapse text-center" id="navbarsExample09">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
+                <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                    <li><a class="dropdown-item" href="about.html">Our company</a></li>
+                    <li><a class="dropdown-item" href="pricing.html">Pricing</a></li>
+                </ul>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="service.html">Services</a></li>
+            <li class="nav-item"><a class="nav-link" href="project.html">Portfolio</a></li>
+            <li class="nav-item"><a class="nav-link" href="blog-grid.php">Blog</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                </ul>
+
+                <form class="form-lg-inline my-2 my-md-0 ml-lg-4 text-center">
+                <a href="contact.html" class="btn btn-solid-border btn-round-full">Get a Quote</a>
+                </form>
+            </div>
+		</div>
+	</nav>
+</header>
+
+<!-- Header Close --> 
+
+<div class="main-wrapper ">
